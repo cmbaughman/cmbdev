5387f3654e7d7b46b58319fbd3a8401b81d38f80
Chris Baughman <cmbaughman@outlook.com>
2024-11-04 14:32:48 -0500

    Testing new form handler

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -1,29 +1,8 @@
 'use client';
 import React, { useState } from "react";
 import { Button } from '@/components/Button';
+import { MailIcon } from "./MailIcon";
 
-function MailIcon(props) {
-  return (
-    <svg
-      viewBox="0 0 24 24"
-      fill="none"
-      strokeWidth="1.5"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      aria-hidden="true"
-      {...props}
-    >
-      <path
-        d="M2.75 7.75a3 3 0 0 1 3-3h12.5a3 3 0 0 1 3 3v8.5a3 3 0 0 1-3 3H5.75a3 3 0 0 1-3-3v-8.5Z"
-        className="fill-zinc-100 stroke-zinc-400 dark:fill-zinc-100/10 dark:stroke-zinc-500"
-      />
-      <path
-        d="m4 6 6.024 5.479a2.915 2.915 0 0 0 3.952 0L20 6"
-        className="stroke-zinc-400 dark:stroke-zinc-500"
-      />
-    </svg>
-  )
-}
 
 export function Subscribe() {
   const [email, setEmail] = useState('');
@@ -34,7 +13,7 @@ export function Subscribe() {
     setError(null);
 
     try {
-      const response = await fetch('/api/subscribe', {
+      const response = await fetch('https://lokhd0x6o9.execute-api.us-east-1.amazonaws.com/default/cmbNinjaSubscribe', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
