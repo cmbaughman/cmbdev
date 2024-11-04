20bc22b7afc5aaad95a7815d3ecbeda9587d4128
Chris Baughman <cmbaughman@outlook.com>
2024-11-04 14:54:25 -0500

    stupid cors

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -22,7 +22,6 @@ export function Subscribe() {
         body: JSON.stringify({ email })
       });
 
-      console.log('test:', JSON.stringify({ email }));
       if (response.ok) {
         window.location.href = '/thank-you'
       }
@@ -57,7 +56,7 @@ export function Subscribe() {
           aria-label="Email address"
           required
           value={ email }
-          onChange={(e) => setEmail(e.target.value)}
+          onChange={(e) => {setEmail(e.target.value); console.log('e ', e.target.value)}}
           className="min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 sm:text-sm dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10"
         />
         <Button type="submit" className="ml-4 flex-none">
